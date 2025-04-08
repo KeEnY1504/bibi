3f1edae6274a236ee375ed95a9252a573c5cfa21
Jojik <egor01020304050607@gmail.com>
2025-04-08 15:17:20 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,9 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename={import.meta.env.BASE_URL}>
-    <App />
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
